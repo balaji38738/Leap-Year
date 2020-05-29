892ea296e8c1ac6160522f9b3535b8858c962661
balaji38738 <bijjapwar123@gmail.com>
2020-05-29 16:51:57 +0530

    ADDED: Code to check leap year

---
+++ Desktop/Python-Programmes/leap_year.py
@@ -0,0 +1,10 @@
+year = int(input("Enter year "))
+if year < 1000 and year > 9999:
+    print("Invalid input")
+else:
+    if year % 400 == 0:
+        print("Leap year")
+    elif (year % 4 == 0) and (year % 100 != 0):
+        print("Leap year")
+    else:
+        print("Not a leap year")
