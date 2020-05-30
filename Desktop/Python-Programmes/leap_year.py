e7e3af79f7d804fbbc68164e3ea7cc68ace11f38
balaji38738 <bijjapwar123@gmail.com>
2020-05-30 19:41:07 +0530

    REFACTORED: Code to avoid violation of single responsibility principle

--- Desktop/Python-Programmes/leap_year.py
+++ Desktop/Python-Programmes/leap_year.py
@@ -1,16 +1,16 @@
-def check_leap_year():
-    try :
-        year = int(input("Enter year "))
-        if year < 1000 or year > 9999:
-            print("Year should be in range 1000-9999")
+def check_leap_year(year):
+    if year < 1000 or year > 9999:
+        print("Year should be in range 1000-9999")
+    else:
+        if ((year % 400 == 0)
+            or ((year % 4 == 0) and (year % 100 != 0))):
+                print("Leap year")
         else:
-            if ((year % 400 == 0)
-                or ((year % 4 == 0) and (year % 100 != 0))):
-                    print("Leap year")
-            else:
-                print("Not a leap year")
-    except ValueError:
-        print("Invalid value")
+            print("Not a leap year")
 
 
-check_leap_year()
+try:
+    year = int(input("Enter year "))
+    check_leap_year(year)
+except ValueError:
+    print("Invalid value")
