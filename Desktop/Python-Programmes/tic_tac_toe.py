3eac21aa75cc6ae2268df0333a54e7ff1ce748fb
balaji38738 <bijjapwar123@gmail.com>
2020-05-30 10:14:52 +0530

    ADDED: Code to reset the board

--- Desktop/Python-Programmes/tic_tac_toe.py
+++ Desktop/Python-Programmes/tic_tac_toe.py
@@ -1,3 +1,18 @@
+import numpy as np
+
 print("-------Tic Tac Toe-------")
 user_char = "x"
 comp_char = "o"
+filled_cells = 0
+
+board = np.empty((3,3), dtype=str)
+
+def reset_board():
+    print("\nNew game starts")
+    global filled_cells
+    filled_cells = 0
+    for row in range(3):
+        for column in range(3):
+            board[row][column] = " "
+
+reset_board()
