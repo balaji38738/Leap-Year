75f69db2f9207001e5912d236ef06c5974bfb6b9
balaji38738 <bijjapwar123@gmail.com>
2020-05-30 15:34:37 +0530

    ADDED: Code for function

--- Desktop/Python-Programmes/leap_year.py
+++ Desktop/Python-Programmes/leap_year.py
@@ -1,10 +1,14 @@
-year = int(input("Enter year "))
-if year < 1000 and year > 9999:
-    print("Invalid input")
-else:
-    if year % 400 == 0:
-        print("Leap year")
-    elif (year % 4 == 0) and (year % 100 != 0):
-        print("Leap year")
+def check_leap_year():
+    year = int(input("Enter year "))
+    if year < 1000 and year > 9999:
+        print("Invalid input")
     else:
-        print("Not a leap year")
+        if year % 400 == 0:
+            print("Leap year")
+        elif (year % 4 == 0) and (year % 100 != 0):
+            print("Leap year")
+        else:
+            print("Not a leap year")
+
+
+check_leap_year()
