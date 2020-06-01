6a51d6e3ca09e550ae2a11cdb154259256993449
balaji38738 <bijjapwar123@gmail.com>
2020-06-01 13:36:57 +0530

    ADDED: Code to start the game

--- Desktop/Python-Programmes/tic_tac_toe.py
+++ Desktop/Python-Programmes/tic_tac_toe.py
@@ -43,11 +43,6 @@ class TicTacToe:
                 print("\n--------")
         print("\n")
 
-    #Starts a new game
-    def play_game(self):
-        print("You are assigned", user_char)
-        self.coin_toss()
-
     #Checks equality of three cells & finds the winner if any
     def are_three_equal(
             self, cell1,
@@ -157,6 +152,7 @@ class TicTacToe:
             is_empty_corner = False
         return is_empty_corner
 
+    #Fills empty side
     def fill_side(self):
         is_empty_side = True
         if board[0, 1] == " ":
@@ -171,7 +167,76 @@ class TicTacToe:
             is_empty_side = False
         return is_empty_side
 
-tic_tac_toe = TicTacToe()
-tic_tac_toe.reset_board()
-tic_tac_toe.display_board()
-tic_tac_toe.play_game()
+    #Takes input from user and fills the cell
+    def fill_user_cell(self):
+
+        #Loop until user gives valid input
+        while True:
+            user_row, user_column = [int(i) for i in input("Enter row (0-2) and column(0-2): ").split()]
+            if board[user_row, user_column] == " ":
+                self.fill_cell(user_char, user_row, user_column)
+                break
+            else:
+                print("The cell is already filled")
+
+    #Starts a new game
+    def play_game(self):
+        print("You are assigned", user_char)
+        self.coin_toss()
+
+        #Loop until the game is finished
+        while True:
+            self.display_board()
+            self.check_if_game_won()
+
+            #If noone won & game not finished
+            if (not self.is_won) and filled_cells != 9:
+                if self.turn == USER:
+                    self.fill_user_cell()
+                else:
+                    print("Computer's turn")
+
+                    #If computer gets the second turn
+                    if filled_cells == 1:
+                        if (board[0, 0] == user_char or board[0, 2] == user_char
+                                or board[2, 0] == user_char or board[2, 2] == user_char):
+                            self.fill_cell(comp_char, 1, 1)
+                            continue
+                    elif filled_cells == 3:
+                        empty_cell_row, empty_cell_column = self.check_win_possibility(user_char)
+                        if empty_cell_row is not None:
+                            self.fill_cell(comp_char, empty_cell_row, empty_cell_column)
+                            continue
+                        else:
+                            self.fill_side()
+                            continue
+                    
+                    #Checks for computer's winnnig possibility
+                    empty_cell_row, empty_cell_column = self.check_win_possibility(comp_char)
+                    if empty_cell_row is not None:
+                        self.fill_cell(comp_char, empty_cell_row, empty_cell_column)
+                    else:
+
+                        #Blocks the user if he can win
+                        empty_cell_row, empty_cell_column = self.check_win_possibility(user_char)
+                        if empty_cell_row is not None:
+                            self.fill_cell(comp_char, empty_cell_row, empty_cell_column)
+                        else:
+
+                            #Priority of filling cells if noone is winning (corner, center, side)
+                            is_empty_corner = self.fill_corner()
+                            if not is_empty_corner:
+                                if board[1, 1] == " ":
+                                    self.fill_cell(comp_char, 1, 1)
+                                else:
+                                    self.fill_side()
+            else:
+                break
+
+
+tic_tac_toe_game = TicTacToe()
+play_again = "y"
+while play_again == "y":
+    tic_tac_toe_game.reset_board()
+    tic_tac_toe_game.play_game()
+    play_again = input("Do you want to play again(y/n) ?: ")
