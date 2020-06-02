34854a04f087ea14133082822f27162ce6afa45b
balaji38738 <bijjapwar123@gmail.com>
2020-06-02 11:08:41 +0530

    ADDED: Code to check employee presence

--- Desktop/Python-Programmes/employee_wage_computation.py
+++ Desktop/Python-Programmes/employee_wage_computation.py
@@ -1,1 +1,12 @@
+import random
+
 print("Welcome to employee wage computation")
+
+#constants
+IS_FULL_TIME = 1
+
+emp_presence = random.randint(0, 1)
+if emp_presence == IS_FULL_TIME:
+    print("Employee is present")
+else:
+    print("Employess is absent")
