b9f2d5f632fbcd47703fddda26d61a1230efa71c
balaji38738 <bijjapwar123@gmail.com>
2020-06-02 16:41:51 +0530

    ADDED: Code to calculate employee wage of mulptiple companies

--- Desktop/Python-Programmes/employee_wage_computation.py
+++ Desktop/Python-Programmes/employee_wage_computation.py
@@ -4,32 +4,43 @@ print("Welcome to employee wage computation")
 
 class EmployeeWageComputation:
         #constants
-        IS_FULL_TIME = 1
-        IS_PART_TIME = 2
-        EMP_RATE_PER_HOUR = 200
-        NUM_WORKING_DAYS = 20
-        MAX_HOURS_IN_MONTH = 120
+        __IS_FULL_TIME = 1
+        __IS_PART_TIME = 2
 
-        #variables
-        emp_wage = total_emp_wage = 0
-        emp_hours = total_emp_hours = total_working_days = 0
+        def __init__(
+                self, emp_rate_per_hour,
+                num_working_days, max_hours_in_month):
+            self.__EMP_RATE_PER_HOUR = emp_rate_per_hour
+            self.__NUM_WORKING_DAYS = num_working_days
+            self.__MAX_HOURS_IN_MONTH = max_hours_in_month
+            self.__total_emp_wage = 0
 
         @staticmethod
-        def compute_emp_wage():
-            #Loop to iterate over working days in month
-            while (__class__.total_working_days < __class__.NUM_WORKING_DAYS
-                        and __class__.total_emp_hours < __class__.MAX_HOURS_IN_MONTH):
-                emp_presence = random.randint(0, 3)
-                if emp_presence == __class__.IS_FULL_TIME:
-                    __class__.emp_hours = 8
-                elif emp_presence == __class__.IS_PART_TIME:
-                    __class__.emp_hours = 4
-                __class__.emp_wage = __class__.emp_hours * __class__.EMP_RATE_PER_HOUR
-                __class__.total_emp_wage += __class__.emp_wage
-                __class__.total_working_days += 1
-                __class__.total_emp_hours += __class__.emp_hours
-                print("Day", __class__.total_working_days, " | Employee hours: ", __class__.emp_hours,
-                        " | Employee wage: ", __class__.emp_wage)
-            print("Total employee wage for month:", __class__.total_emp_wage)
+        def compute_emp_wage(company):
 
-EmployeeWageComputation.compute_emp_wage()
+            #variables
+            emp_hours = total_emp_hours = 0
+            total_working_days = emp_wage = 0
+
+            #Loop to iterate over working days in month
+            while (total_working_days < company.__NUM_WORKING_DAYS
+                        and total_emp_hours < company.__MAX_HOURS_IN_MONTH):
+                emp_presence = random.randint(0, 3)
+                if emp_presence == __class__.__IS_FULL_TIME:
+                    emp_hours = 8
+                elif emp_presence == __class__.__IS_PART_TIME:
+                    emp_hours = 4
+                emp_wage = emp_hours * company.__EMP_RATE_PER_HOUR
+                company.__total_emp_wage += emp_wage
+                total_working_days += 1
+                total_emp_hours += emp_hours
+                print("Day", total_working_days, " | Employee hours: ", emp_hours,
+                        " | Employee wage: ", emp_wage)
+            print("Total employee wage for month:", company.__total_emp_wage)
+
+emp_wage_computation1 = EmployeeWageComputation(200, 20, 120)
+emp_wage_computation2 = EmployeeWageComputation(300, 25, 110)
+print("Employee wage of first company")
+EmployeeWageComputation.compute_emp_wage(emp_wage_computation1)
+print("\nEmployee wage of second company")
+EmployeeWageComputation.compute_emp_wage(emp_wage_computation2)
