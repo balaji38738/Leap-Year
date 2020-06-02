ad4bbd467ed7ebe41e9ddf77cc879b34b54ac788
balaji38738 <bijjapwar123@gmail.com>
2020-06-02 11:14:51 +0530

    ADDED: Code to calculate full time employee wage

--- Desktop/Python-Programmes/employee_wage_computation.py
+++ Desktop/Python-Programmes/employee_wage_computation.py
@@ -4,9 +4,13 @@ print("Welcome to employee wage computation")
 
 #constants
 IS_FULL_TIME = 1
+EMP_RATE_PER_HOUR = 200
+
+#variables
+emp_hours, emp_wage = 0, 0
 
 emp_presence = random.randint(0, 1)
 if emp_presence == IS_FULL_TIME:
-    print("Employee is present")
-else:
-    print("Employess is absent")
+    emp_hours = 8
+emp_wage = emp_hours * EMP_RATE_PER_HOUR
+print("Employee wage:", emp_wage)
