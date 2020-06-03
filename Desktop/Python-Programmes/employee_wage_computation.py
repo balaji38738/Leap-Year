9d14cb0b6388e776bacdc2764d40a0030d9f41ba
balaji38738 <bijjapwar123@gmail.com>
2020-06-03 20:05:10 +0530

    ADDED: Code to store employee daily wage

--- Desktop/Python-Programmes/employee_wage_computation.py
+++ Desktop/Python-Programmes/employee_wage_computation.py
@@ -20,38 +20,44 @@ class EmployeeWageComputation(Computable):
         __IS_PART_TIME = 2
 
         @staticmethod
-        def compute_emp_wage(employee):
-            MAX_HOURS_IN_MONTH = employee.get_max_hours_in_month()
-            NUM_WORKING_DAYS = employee.get_num_working_days()
-            EMP_RATE_PER_HOUR = employee.get_emp_rate_per_hour()
+        def compute_emp_wage(employee_list):
+            company_no = 0
+            for employee in employee_list:
+                company_no += 1
+                MAX_HOURS_IN_MONTH = employee.get_max_hours_in_month()
+                NUM_WORKING_DAYS = employee.get_num_working_days()
+                EMP_RATE_PER_HOUR = employee.get_emp_rate_per_hour()
 
-            #variables
-            emp_hours = total_emp_hours = 0
-            total_working_days = emp_wage = 0
-            total_emp_wage = 0
+                #variables
+                emp_hours = total_emp_hours = 0
+                total_working_days = emp_wage = 0
+                total_emp_wage = 0
 
-            #Loop to iterate over working days in month
-            while (total_working_days < NUM_WORKING_DAYS
-                        and total_emp_hours < MAX_HOURS_IN_MONTH):
-                emp_presence = random.randint(0, 3)
-                if emp_presence == __class__.__IS_FULL_TIME:
-                    emp_hours = 8
-                elif emp_presence == __class__.__IS_PART_TIME:
-                    emp_hours = 4
-                emp_wage = emp_hours * EMP_RATE_PER_HOUR
-                total_emp_wage += emp_wage
-                total_working_days += 1
-                total_emp_hours += emp_hours
-                __class__.display_daily_wage(total_working_days, emp_hours,
-                                            emp_wage)
-            employee.set_total_emp_wage(total_emp_wage)
+                #Loop to iterate over working days in month
+                while (total_working_days < NUM_WORKING_DAYS
+                            and total_emp_hours < MAX_HOURS_IN_MONTH):
+                    emp_presence = random.randint(0, 3)
+                    if emp_presence == __class__.__IS_FULL_TIME:
+                        emp_hours = 8
+                    elif emp_presence == __class__.__IS_PART_TIME:
+                        emp_hours = 4
+                    emp_wage = emp_hours * EMP_RATE_PER_HOUR
+                    employee.add_daily_wage(emp_wage)
+                    total_emp_wage += emp_wage
+                    total_working_days += 1
+                    total_emp_hours += emp_hours
+                employee.set_total_emp_wage(total_emp_wage)
         
         @staticmethod
-        def display_daily_wage(
-                total_working_days, emp_hours,
-                emp_wage):
-            print("Day", total_working_days, " | Employee hours: ", emp_hours,
-                        " | Employee wage: ", emp_wage)
+        def display_employee_wage(employee_list):
+            employee_no = 0
+            for employee in employee_list:
+                employee_no += 1
+                print("\nEmployee wage of employee", employee_no)
+                daily_wage = employee.get_daily_wage()
+                for day in range(0, len(daily_wage)):
+                    print("day", day, ":" , daily_wage[day])
+                print("Employee monthly wage", employee.get_total_emp_wage())
 
 
 class CompanyEmployee:
@@ -62,6 +68,7 @@ class CompanyEmployee:
         self.__NUM_WORKING_DAYS = num_working_days
         self.__MAX_HOURS_IN_MONTH = max_hours_in_month
         self.__total_emp_wage = 0
+        self.__daily_wages = []
 
     def get_emp_rate_per_hour(self):
         return self.__EMP_RATE_PER_HOUR
@@ -78,10 +85,16 @@ class CompanyEmployee:
     def set_total_emp_wage(self, total_emp_wage):
         self.__total_emp_wage = total_emp_wage
 
+    def add_daily_wage(self, wage):
+        self.__daily_wages.append(wage)
+    
+    def get_daily_wage(self):
+        return self.__daily_wages
 
-TOTAL_COMPANIES = 10
+
+user_input = "y"
 employee_list = []
-for company_no in range(0, TOTAL_COMPANIES):
+while user_input == "y":
     #Generate Employee rate between 100 to 300
     emp_rate_per_hour = random.randint(100, 300)
 
@@ -91,9 +104,10 @@ for company_no in range(0, TOTAL_COMPANIES):
     #Generate Employee work hours between 100 to 150
     max_hours_in_month = random.randint(100, 150)
     
-    company_employee = CompanyEmployee(emp_rate_per_hour,num_working_days,
+    company_employee = CompanyEmployee(emp_rate_per_hour, num_working_days,
                                         max_hours_in_month)
     employee_list.append(company_employee)
-    print("\nEmployee wage of company", company_no + 1)
-    EmployeeWageComputation.compute_emp_wage(company_employee)
-    print("Total employee wage for month:", company_employee.get_total_emp_wage())
+    user_input = input("Employee added, do you want to add more ?(y/n): ")
+
+EmployeeWageComputation.compute_emp_wage(employee_list)
+EmployeeWageComputation.display_employee_wage(employee_list)
