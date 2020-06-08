b6650767a4ab946eb9a987685d04636a7a2c0882
balaji38738 <bijjapwar123@gmail.com>
2020-06-09 00:34:01 +0530

    ADDED: SnakeAndLadder class & code to print random dice value

--- Desktop/Python-Programmes/snake_and_ladder.py
+++ Desktop/Python-Programmes/snake_and_ladder.py
@@ -1,4 +1,17 @@
+import random
+
 print("Snake And Ladder")
 print("----------------")
 START = 0
 print("Starting position is", START)
+
+class SnakeAndLadder:
+    def dice_roll(self):
+        return random.randint(1, 6)
+
+    def print_dice_value(self):
+        print(f"Dice value is: {self.dice_roll()}")
+
+snake_ladder = SnakeAndLadder()
+snake_ladder.print_dice_value()
+    
