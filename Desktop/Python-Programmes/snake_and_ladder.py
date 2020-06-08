7de435587275958b0be35828a82efb228af0f99f
balaji38738 <bijjapwar123@gmail.com>
2020-06-09 01:21:00 +0530

    ADDED: Player class

--- Desktop/Python-Programmes/snake_and_ladder.py
+++ Desktop/Python-Programmes/snake_and_ladder.py
@@ -12,6 +12,25 @@ class SnakeAndLadder:
     def print_dice_value(self):
         print(f"Dice value is: {self.dice_roll()}")
 
+class Player:
+    def __init__(self, name):
+        self.__position = START
+        self.__name = name
+
+    def get_position(self):
+        return self.__position
+
+    def set_position(self, position):
+        if position >= 0 and position <= 100:
+            self.__position = position
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+
 snake_ladder = SnakeAndLadder()
 snake_ladder.print_dice_value()
     
