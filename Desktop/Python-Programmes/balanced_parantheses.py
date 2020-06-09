fc43e272e134914e2db7a546a082f19572d96250
balaji38738 <bijjapwar123@gmail.com>
2020-06-09 11:06:50 +0530

    ADDED: Code to check for balanced parantheses

---
+++ Desktop/Python-Programmes/balanced_parantheses.py
@@ -0,0 +1,33 @@
+class BalancedParanthesesChecker:
+    brackets = {"{": "}", "[": "]", "(": ")"}
+    brackets_set = set(brackets.keys()).union(set(brackets.values()))
+
+    @classmethod
+    def is_valid_expression(cls, expression):
+        if set(expression).issubset(cls.brackets_set):
+            return True
+        else:
+            return False
+
+    @classmethod
+    def is_balanced(cls, expression):
+        stack = []
+        for bracket in expression:
+            if bracket in cls.brackets.keys():
+                stack.append(bracket)
+            elif cls.brackets[stack[-1]] == bracket:
+                stack.pop()
+            else:
+                return False
+        else:
+            return True
+    
+expression = input("Enter parantheses expression: ")
+if BalancedParanthesesChecker.is_valid_expression(expression):
+    if BalancedParanthesesChecker.is_balanced(expression):
+        print("Balanced parantheses")
+    else:
+        print("Unbalanced parantheses")
+else:
+    print("Expression should only contain", BalancedParanthesesChecker.brackets_set)
+
