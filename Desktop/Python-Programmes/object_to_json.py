e723c64c9dbba998ff11ad6956e92ad12fd3b6c8
balaji38738 <bijjapwar123@gmail.com>
2020-06-12 11:11:17 +0530

    Balaji Ijjapwar Add. Code to sort json keys

--- Desktop/Python-Programmes/object_to_json.py
+++ Desktop/Python-Programmes/object_to_json.py
@@ -1,4 +1,6 @@
 import json
 
-student = {"Name": "Balaji", "Branch": "E&Tc", "Subjects": ["Electromagnetics", "LARV", "Digital Electronics"]}
-print(json.dumps(student, indent=4, separators=(".", ": ")))
+student = {"Name": "Balaji", "Branch": "E&Tc", "Subjects": ["Electromagnetics", "LARV", "Digital Electronics"],
+             "Roll No": 21}
+print(json.dumps(student, indent=4, separators=(",", ": ")))
+print(json.dumps(student, indent=4, sort_keys=True))
