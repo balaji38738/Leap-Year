098513a59f2207b711cf8d9cb883ecbfc0db3d1e
balaji38738 <bijjapwar123@gmail.com>
2020-06-14 05:10:52 +0530

    Balaji Ijjapwar Add. Code to write patient data into json file

--- Desktop/Python-Programmes/Clinic Management/clinic_management.py
+++ Desktop/Python-Programmes/Clinic Management/clinic_management.py
@@ -1,8 +1,22 @@
 import json
 
-class FileReader:
-    def __init__(self, file_path):
-        with open(file_path) as doctor_file:
-            self.file_data = json.load(doctor_file)
+class FileHandler:
+    def read_file(self, file_path):
+        with open(file_path) as clinique_file:
+            file_data = json.load(clinique_file)
+            return file_data
 
-doctor_data = FileReader("doctor_data.json")
+    def write_file(self, file_path, new_entry):
+        file_data = self.read_file(file_path)
+        with open(file_path, "w") as clinique_file:
+            file_data.append(json.dumps(new_entry))
+            json.dump(file_data, clinique_file, indent=4)
+
+
+file_hanler = FileHandler()
+doctor_data = file_hanler.read_file("doctor_data.json")
+print(doctor_data)
+patient = {"Patient Name": "Mahesh Munde", "Id": 1, "Mobile": 7588842796, "Age": 24}
+file_hanler.write_file("patient_data.json", patient)
+patient_data = file_hanler.read_file("patient_data.json")
+print(patient_data)
