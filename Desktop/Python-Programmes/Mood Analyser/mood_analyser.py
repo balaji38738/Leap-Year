62f725efe4d0da54b8713168bd65bea0ede0ded5
balaji38738 <bijjapwar123@gmail.com>
2020-06-14 10:56:23 +0530

    Balaji Ijjapwar Refactor. Code to make message as input instead of mood

--- Desktop/Python-Programmes/Mood Analyser/mood_analyser.py
+++ Desktop/Python-Programmes/Mood Analyser/mood_analyser.py
@@ -2,25 +2,23 @@ import json
 
 class FileHandler:
     def read_file(self, file_path):
-        with open(file_path) as moods_file:
-            moods = json.load(moods_file)
-        return moods
+        with open(file_path) as message_file:
+            messages = json.load(message_file)
+        return messages
 
 
 class MoodAnalyser:
-    def __init__(self, file_path):
-        self.file_handler = FileHandler()
-        self.moods = self.file_handler.read_file(file_path)
-
     def analyse_mood(self, message):
-        if message.find(self.moods[0]["mood"]) != -1:
+        if message.find("happy") != -1:
             return "happy"
-        elif message.find(self.moods[1]["mood"]) != -1:
+        elif message.find("sad") != -1:
             return "sad"
         else:
             return "Invalid mood"
 
-mood_analyser = MoodAnalyser("moods.json")
-print(mood_analyser.analyse_mood("I am happy"))
-print(mood_analyser.analyse_mood("I am sad"))  
-print(mood_analyser.analyse_mood("I am angry"))     
+
+messages = FileHandler().read_file("messages.json")
+mood_analyser = MoodAnalyser()
+for message in messages:
+    mood = mood_analyser.analyse_mood(message["message"])
+    print(mood)
