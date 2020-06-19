eb42f2b885b399b34d67576800ef47f87832c850
balaji38738 <bijjapwar123@gmail.com>
2020-06-20 05:00:27 +0530

    Balaji Ijjapwar Add. Test to catch CensusAnalyserError given wrong delimiter file

--- Desktop/Python-Programmes/Census-Analyser/census_analyser_exception.py
+++ Desktop/Python-Programmes/Census-Analyser/census_analyser_exception.py
@@ -3,6 +3,7 @@ import enum
 class CensusAnalyserError(Exception):
     class ExceptionType(enum.Enum):
         FILE_NOT_FOUND = 1
+        WRONG_DELIMITER = 2
 
     def __init__(self, exception_type, message):
         self.message = message
--- Desktop/Python-Programmes/Census-Analyser/test_census_analyser.py
+++ Desktop/Python-Programmes/Census-Analyser/test_census_analyser.py
@@ -4,6 +4,7 @@ from census_analyser_exception import CensusAnalyserError
 class TestCensusAnalyser:
     CENSUS_CSV_FILE_PATH = "IndiaStateCensusData.csv"
     CENSUS_WRONG_CSV_FILE_PATH = "/home/IndiaStateCensusData.csv"
+    CENSUS_WRONG_DELIMITER_FILE = "CensusInvalidDelimiter.csv"
 
     def test_given_indian_census_CSV_file_returns_correct_records(self):
         census_analyser = CensusAnalyser()
@@ -16,5 +17,13 @@ class TestCensusAnalyser:
             census_analyser = CensusAnalyser()
             num_of_records = census_analyser.load_india_census_data(
                             TestCensusAnalyser.CENSUS_CSV_FILE_PATH)
+        except CensusAnalyserError as exception:
+            assert exception.exception_type == CensusAnalyserError.ExceptionType.FILE_NOT_FOUND
+
+    def test_given_india_census_file_with_wrong_delimiter_should_throw_exception(self):
+        try:
+            census_analyser = CensusAnalyser()
+            num_of_records = census_analyser.load_india_census_data(
+                            TestCensusAnalyser.CENSUS_WRONG_DELIMITER_FILE)
         except CensusAnalyserError as exception:
             assert exception.exception_type == CensusAnalyserError.ExceptionType.FILE_NOT_FOUND
