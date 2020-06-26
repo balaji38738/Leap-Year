758790d12d11dac4cfe3e8be60f5c401cc484687
balaji38738 <bijjapwar123@gmail.com>
2020-06-27 00:35:50 +0530

    [Balaji] Moved text files into separate folder

--- Desktop/Python-Programmes/Matplotlib/live graphs.py
+++ Desktop/Python-Programmes/Matplotlib/live graphs.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation, style
+
 #use style.available to print all graph style options
 #can pass a list of styles as well
 style.use('Solarize_Light2')
@@ -8,7 +9,7 @@ ax = fig.add_subplot(1, 1, 1)
 
 #Plots animated graph
 def animate(i):
-    file = open('example.txt', 'r').read()
+    file = open('Resources/example.txt', 'r').read()
     lines = file.split('\n')
     nums, squares = [], []
     for line in lines:
