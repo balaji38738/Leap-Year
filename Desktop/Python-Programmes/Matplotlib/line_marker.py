ed471c5ea9b9be913af6c8410b0ea5afa63f6593
balaji38738 <bijjapwar123@gmail.com>
2020-06-27 16:04:18 +0530

    [Balaji] Add.Code for line marker

--- Desktop/Python-Programmes/Matplotlib/line_marker.py
+++ Desktop/Python-Programmes/Matplotlib/line_marker.py
@@ -4,18 +4,20 @@ class StraightLine:
 
     # Method to draw straight line
     @staticmethod
-    def draw_line(xs, ys, label_, color_, linestyle_):
-        plt.plot(xs, ys, label=label_, color=color_, linestyle=linestyle_)
+    def draw_line(xs, ys, label_, color_, linestyle_, marker_):
+        plt.plot(xs, ys, label=label_, color=color_,
+                linestyle=linestyle_, marker=marker_)
 
     @staticmethod  
     def label_and_show():
         plt.xlabel('x-axis')
         plt.ylabel('y-axis')
-        plt.title('Dashed & dotted line graph')
+        plt.title('Graph of y=2x, y=3x & y=-x')
         plt.legend()
         plt.show()
 
 straight_line = StraightLine()
-straight_line.draw_line([1, 2], [2, 4], "y=2x", "m", "dashed")
-straight_line.draw_line([1, 2], [3, 6], "y=3x", "b", "dotted")
+straight_line.draw_line([1, 2], [2, 4], "y=2x", "m", "dashed", "*")
+straight_line.draw_line([1, 2], [3, 6], "y=3x", "b", "dotted", "+")
+straight_line.draw_line([1, 2], [-1, -2], "y=-x", "g", "solid", "x")
 straight_line.label_and_show()
