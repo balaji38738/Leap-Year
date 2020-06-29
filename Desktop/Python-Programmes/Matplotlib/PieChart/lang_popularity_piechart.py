a3e2b4a04fe933a38e9e77a4b719a7304dec10e8
balaji38738 <bijjapwar123@gmail.com>
2020-06-29 11:47:34 +0530

    [Balaji] Add.Code to draw pie chart for programming language popularity

---
+++ Desktop/Python-Programmes/Matplotlib/PieChart/lang_popularity_piechart.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+class PieChart:
+
+    # Draws the pie chart
+    @staticmethod
+    def draw_chart(slice_names, slice_values, colors_, title_):
+        plt.pie(
+                slice_values,
+                labels=slice_names,
+                startangle=90,
+                explode=(0, 0.1, 0, 0, 0, 0),
+                autopct='%.2f%%',
+                shadow=True)
+        plt.title(title_)
+        plt.show()
+
+if __name__ == "__main__":
+    # Data
+    prog_language = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+    popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+
+    #Plot graph
+    colors = ["r", "g", "b", "y", "c", "m"]
+    bar_chart = PieChart()
+    bar_chart.draw_chart(prog_language, popularity,
+                        colors, "Programming Language popularity chart")
