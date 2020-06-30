ffc6e1e9c4e2b216149e7e577d7b03f18453e9e0
balaji38738 <bijjapwar123@gmail.com>
2020-06-30 10:34:21 +0530

    [Balaji] Add.Code to for swarm plot of day against total bill in tips dataset

---
+++ Desktop/Python-Programmes/Seaborn/total_bill_vs_day.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pandas as pd
+
+class SeabornSwarmPlot:
+    
+    # Draws seaborn box plot
+    @staticmethod
+    def draw_plot(x_column, y_column, dataset, title_):
+        sns.swarmplot(x=x_column, y=y_column, data=dataset)
+        plt.title(title_) 
+        plt.show()
+
+
+if __name__ == "__main__":
+    tips = pd.read_csv("https://raw.githubusercontent.com"
+                    + "//mwaskom//seaborn-data//master//tips.csv")
+
+    SeabornSwarmPlot.draw_plot("day", "total_bill", tips,
+                        "Size against total bill")
