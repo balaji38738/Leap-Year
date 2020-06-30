cecc12498979db8c8f88defefa5803bfd450381d
balaji38738 <bijjapwar123@gmail.com>
2020-06-30 09:36:44 +0530

    [Balaji] Add.Code for seaborn violin of tips dataset

--- Desktop/Python-Programmes/Seaborn/tips_violinplot.py
+++ Desktop/Python-Programmes/Seaborn/tips_violinplot.py
@@ -1,19 +1,21 @@
-import pandas as pd
 import matplotlib.pyplot as plt 
-import seaborn as sns 
+import seaborn as sns
+import pandas as pd
 
-class SeabornScatterPlot:
+class SeabornViolinPlot:
     
-    # Draws seaborn scatter plot
+    # Draws seaborn violin plot
     @staticmethod
     def draw_plot(x_column, y_column, dataset, title_):
         sns.set(style ="whitegrid") 
-        sns.scatterplot(x=x_column, y=y_column, data=dataset)
+        sns.violinplot(x=x_column, y=y_column, data=dataset)
         plt.title(title_) 
         plt.show()
 
 
 if __name__ == "__main__":
     tips = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv") 
-    SeabornScatterPlot.draw_plot("day", "total_bill", tips, "Day against total bill")
+
+    SeabornViolinPlot.draw_plot("sex", "total_bill", tips,
+                        "sex against total bill")
 
