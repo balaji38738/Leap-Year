00565f83fd717a05801327b99b5e7c7ad62e9da9
balaji38738 <bijjapwar123@gmail.com>
2020-07-02 21:51:33 +0530

    [Balaji] Add.Code for hello world REST API

---
+++ Desktop/Python-Programmes/REST API/hello_world.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return jsonify({"about": "Hello, world!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
